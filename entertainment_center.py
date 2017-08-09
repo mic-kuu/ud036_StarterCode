632a1d200daad34ef2aa715d0cb0f2eec70e952f
Michal <kubiakmichal@ymail.com>
2017-08-09 20:43:27 +0200

    Corrections after udacity's review

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,15 +6,15 @@ import website_generator
 
 blade_runner = media.Movie("Blade Runner",
                            "http://i.imgur.com/KykTdlk.jpg",
-                           "https://www.youtube.com/embed/eogpIG53Cis?rel=0&amp;showinfo=0")
+                           "https://www.youtube.com/embed/eogpIG53Cis?rel=0&amp;showinfo=0")  # noqa
 
 fight_club = media.Movie("Fight Club",
                          "http://i.imgur.com/wyboozD.jpg",
-                         "https://www.youtube.com/embed/BdJKm16Co6M?rel=0&amp;showinfo=0")
+                         "https://www.youtube.com/embed/BdJKm16Co6M?rel=0&amp;showinfo=0")  # noqa
 
 kill_bill = media.Movie("Kill Bill",
                         "http://i.imgur.com/5AgF0Zi.jpg",
-                        "https://www.youtube.com/embed/7kSuas6mRpk?rel=0&amp;showinfo=0")
+                        "https://www.youtube.com/embed/7kSuas6mRpk?rel=0&amp;showinfo=0")  # noqa
 
 # Array of movies that will be used to generate website from
 movies = [blade_runner, fight_club, kill_bill]
--- media.py
+++ media.py
@@ -1,6 +1,16 @@
-# The movie class definition
 class Movie():
+    """ Movie class is a representation of a single movie
+
+    An instance object of Movie class can be initialized
+    using the following list of arguments:
+
+    Args:
+        movie_title (str): Title of the movie.
+        poster_url (str): The url to the the poster graphics.
+        trailer_url (str): The url to the youtube video.
+
+    """
     def __init__(self, movie_tile, poster_url, trailer_url):
         self.title = movie_tile
         self.poster_image_url = poster_url
-        self.trailer_youtube_url = trailer_url
+        self.trailer_youtu
